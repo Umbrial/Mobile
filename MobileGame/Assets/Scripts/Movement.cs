537a4ec381e2100102cb3f4f057f6d754276bf77
Brenna Loftus <18.brenna.loftus@ksd.org>
2018-01-29 10:26:51 -0800

    Added my dev scene and am toying with ideas

---
+++ MobileGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //horizontal stores the user input when they press:
+        //a, d, left, right on a scale from -1 to 1
+        float horizontal = Input.GetAxis("Horizontal");
+        //Debug.Log (horizontal);
+
+        //vertical stores the user input when they press:
+        //w, s, up, down on a scale from -1 to 1
+        float vertical = Input.GetAxis("Vertical");
+        //Debug.Log (vertical);
+
+        //create a push variable that combines the user input
+        //This will actually allow you to move
+        Vector2 push = new Vector2(horizontal, vertical);
+        //use push to change the velocity of the object
+        //making you in fact move
+        gameObject.GetComponent<Rigidbody2D>().velocity = push * moveSpeed;
+
+
+    }
+}
