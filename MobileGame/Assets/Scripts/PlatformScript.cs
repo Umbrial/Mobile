01976be8587369c2473c919305559cf6a489a4d7
Brenna Loftus <18.brenna.loftus@ksd.org>
2018-02-12 08:19:19 -0800

    Sprite work, pickups and tiles. BL

---
+++ MobileGame/Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+
+    //using a player pref to keep a boolean
+    //0 means false, 1 means true
+    //Keeping track of if the user is on PC
+    //Or mobile, to disable the mobile controls
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("OnPC", 0);
+    }
+
+    public void PC()
+    {
+        PlayerPrefs.SetInt("OnPc", 1);
+    }
+
+    public void NotPC ()
+    {
+        PlayerPrefs.SetInt("OnOc", 0);
+    }
+}
